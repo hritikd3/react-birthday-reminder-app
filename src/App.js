2cc597dc5cd582cf8e77d43deedb561806febdc1
hritik <86347286+hritikd3@users.noreply.github.com>
2022-02-07 17:32:31 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./styles.css";
+import React, { useState } from "react";
+import data from "./data";
+import List from "./List";
+function App() {
+  const [friends, setFriends] = useState(data);
+  return (
+    <main>
+      <section className="container">
+        <h3>{friends.length} birthdays today</h3>
+        <List friends={friends} />
+        <button onClick={() => setFriends([])}>clear all</button>
+        {/* console.log("click is wrking") */}
+      </section>
+    </main>
+  );
+}
+
+export default App;
